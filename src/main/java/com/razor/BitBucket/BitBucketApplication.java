a72a27fa6051a477a723071633bb70837856afbd
Musharraf1128 <shahmusharaf1128@gmail.com>
2026-01-26 00:10:39 +0530

    setup: completed

---
+++ src/main/java/com/razor/BitBucket/BitBucketApplication.java
@@ -0,0 +1,13 @@
+package com.razor.BitBucket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitBucketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitBucketApplication.class, args);
+	}
+
+}
